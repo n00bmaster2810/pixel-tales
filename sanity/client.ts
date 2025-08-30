8f537a3890e580e3cbc5b11193a3ec5c4c25a699
n00bmaster <satty9509@gmail.com>
2025-08-30 08:46:30 +0530

    moved to env

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,9 +1,9 @@
 import {createClient} from "@sanity/client"
 
 export const client = createClient({
-    projectId: "ews3httd",
-    dataset: "production",
-    apiVersion: "2024-01-01",
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    dataset: import.meta.env.VITE_DATASET,
+    apiVersion: import.meta.env.VITE_API_VERSION,
     useCdn: true, // Enable CDN for faster reads
     perspective: "published", // Only fetch published content
     token: import.meta.env.VITE_SANITY_TOKEN, // Use Vite's env variable
